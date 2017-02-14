35b7e16fd0cbddbf59441ec5f7726bdaa8e66c03
Asad <mehdia@rpi.edu>
2017-02-14 15:49:12 -0500

    Install Mantis, add README.txt, add screenshot of admin panel for Mantis

---
+++ Labs/Lab3/README.txt
@@ -0,0 +1,10 @@
+Asad Mehdi
+Lab 3: Git & Bugs
+
+Individual:
+- The URL for my GitHub repository is here: https://github.com/asadmehdi785/websci
+- The attached screenshot mantis.png shows my admin panel for Mantis, the bug tracking software I installed on my machine.
+
+Group:
+- 
+-
---
+++ mantisbt-2.1.0/config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'America/New_York';
+
+$g_crypto_master_salt     = 'dO06t/rS0HXvUgs4HXDpFEGnZ/y+Bevc1VoU5S160Fw=';
